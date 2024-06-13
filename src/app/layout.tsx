afc9fe2d797fab48cb620cf78fb4e5713a4deb40
Nicolasvegam <ndvega@uc.cl>
2024-06-13 00:44:00 -0400

    feat: favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-            <body className={`${inter.className} flex items-center justify-center min-h-screen bg-gray-800`}>
-
-        {children}</body>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <body
+        className={`${inter.className} flex items-center justify-center min-h-screen bg-gray-800`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
