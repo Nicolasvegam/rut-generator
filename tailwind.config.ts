4b9e4ab787165824cc9e5ff20bc2f4aff76c2033
Nicolasvegam <ndvega@uc.cl>
2025-01-11 15:13:24 -0300

    bug: tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -85,22 +85,6 @@ const config = {
             transform: "translateY(0) translateX(-50%)",
           },
         },
-        "accordion-down": {
-          from: {
-            height: "0",
-          },
-          to: {
-            height: "var(--radix-accordion-content-height)",
-          },
-        },
-        "accordion-up": {
-          from: {
-            height: "var(--radix-accordion-content-height)",
-          },
-          to: {
-            height: "0",
-          },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
